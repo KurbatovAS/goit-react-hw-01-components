7b491cb7dec4d150197d28a3b5b69f8cf5a5cdd2
Andrey Kurbatov <85691791+KurbatovAS@users.noreply.github.com>
2022-02-05 12:47:24 +0200

    Update "propTypes'

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -22,6 +22,11 @@ export default function Statistics({ title, stats }) {
 };
 
 Statistics.propTypes = {
-  stats: PropTypes.array.isRequired,
-  title: PropTypes.string,
+    title: PropTypes.string,
+    stats: PropTypes.arrayOf(
+            PropTypes.shape({
+                id: PropTypes.string.isRequired,
+                label: PropTypes.string.isRequired,
+                percentage: PropTypes.number.isRequired,
+            }))  
 };
